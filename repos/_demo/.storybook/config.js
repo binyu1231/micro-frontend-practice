90a577fb901fc1513759610b37bfc49ad1d06b4c
WangBinyu <406279919@qq.com>
2019-11-05 20:16:57 +0800

    save

--- repos/_demo/.storybook/config.js
+++ repos/_demo/.storybook/config.js
@@ -1,4 +1,6 @@
 import { configure } from '@storybook/react'
+import { configure as htmlConfigure } from '@storybook/html'
 import './global-style.css'
 
 configure(require.context('../storybook', true, /\.stories\.js$/), module)
+htmlConfigure(require.context('../storybook', true, /\.html-stories\.js$/), module)
