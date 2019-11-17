44fa7ea27e54945a07802e00e4e9e3f29fbf34e8
WangBinyu <406279919@qq.com>
2019-11-17 17:21:01 +0800

    save

---
+++ repos/chart/color/pureCategory.tsx
@@ -0,0 +1,34 @@
+export const pureCategory10_1 = [
+]
+
+export const pureCategory10_2 = [
+
+]
+
+export const pureCategory10_3 = [
+
+]
+
+export const pureCategory20_1 = [
+
+]
+
+export const pureCategory20_2 = [
+
+]
+
+export const pureCategory20_3 = [
+  
+]
+
+export const pureCategory30_1 = [
+
+]
+
+export const pureCategory30_2 = [
+
+]
+
+export const pureCategory30_3 = [
+  
+]
