9cec18b4e25f586f50fab8c00495f21f4229354d
WangBinyu <406279919@qq.com>
2020-05-27 00:21:29 +0800

    添加 single spa 并独立运行

--- packages/container-vue/src/main.ts
+++ packages/container-vue/src/main.ts
@@ -2,11 +2,38 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import singleSpaVue from 'single-spa-vue'
 
 Vue.config.productionTip = false
 
-new Vue({
+const vueOptions = {
+  el: '#vue',
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h: any) => h(App)
+}
+
+
+if (!window['singleSpaNavigate' as any]) { // 如果不是single-spa模式
+  delete vueOptions.el;
+  new Vue(vueOptions).$mount('#vue');
+}
+
+const vueLifecycles = singleSpaVue({
+  Vue,
+  appOptions: vueOptions
+})
+
+export const bootstrap = [
+  vueLifecycles.bootstrap
+]
+
+export const mount = [
+  vueLifecycles.mount
+]
+
+export const unmount = [
+  vueLifecycles.unmount
+]
+
+export default vueLifecycles
