b87a8e683dcc353076f9eeabf27c1690b1fbae92
WangBinyu <406279919@qq.com>
2020-06-11 18:10:05 +0800

    angular 9 初始化

--- packages/container-angular/__tests__/container-angular.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const containerAngular = require('..');
-
-describe('container-angular', () => {
-    it('needs tests');
-});
--- packages/container-angular/lib/container-angular.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = containerAngular;
-
-function containerAngular() {
-    // TODO
-}
